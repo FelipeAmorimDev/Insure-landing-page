472c61e04e9fda6cfa04e2d799e33427da22127c
FelipeAmorimDev <felipeamorim.go@gmail.com>
2022-10-17 02:38:57 -0300

    Website finalizado

--- script/script.js
+++ script/script.js
@@ -1,16 +1,16 @@
 const menu = document.getElementById("botao");
 
 
-function toogleMenu(){
+function toogleMenu() {
   const nav = document.getElementById("nav-menu-c");
   nav.classList.toggle("active")
   var toogleActive = nav.classList.contains("active");
-  if(toogleActive){
-    menu.style.backgroundImage="url(./images/icon-close.svg)"
+  if (toogleActive) {
+    menu.style.backgroundImage = "url(./images/icon-close.svg)"
   }
-  else{
-    menu.style.backgroundImage="url(./images/icon-hamburger.svg)"
+  else {
+    menu.style.backgroundImage = "url(./images/icon-hamburger.svg)"
   }
 }
 
-menu.addEventListener("click",toogleMenu)
+menu.addEventListener("click", toogleMenu)
