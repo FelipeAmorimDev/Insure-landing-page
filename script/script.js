1424218444576fec6672e6ebf85c2bd14ad8ae7d
FelipeAmorimDev <felipeamorim.go@gmail.com>
2022-10-17 01:30:08 -0300

    Adicionado alguns detalhes

--- script/script.js
+++ script/script.js
@@ -4,6 +4,13 @@ const menu = document.getElementById("botao");
 function toogleMenu(){
   const nav = document.getElementById("nav-menu-c");
   nav.classList.toggle("active")
+  var toogleActive = nav.classList.contains("active");
+  if(toogleActive){
+    menu.style.backgroundImage="url(../../images/icon-close.svg"
+  }
+  else{
+    menu.style.backgroundImage="url(../../images/icon-hamburger.svg"
+  }
 }
 
 menu.addEventListener("click",toogleMenu)
